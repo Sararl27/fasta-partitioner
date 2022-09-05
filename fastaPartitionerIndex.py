b613e09360dd9515283633434ff5b08378f041d0
Sararl27 <85760827+Sararl27@users.noreply.github.com>
2022-09-05 13:10:40 +0200

    Update fastaPartitionerIndex.py

--- fastaPartitionerIndex.py
+++ fastaPartitionerIndex.py
@@ -89,45 +89,46 @@ class FastaPartitioner:
     def __reduce_generate_chunks(self, results):
         if len(results) > 1:
             for i, dict in enumerate(results):
-                dictio = dict['sequences']
-                dict_prev = results[i - 1]
-                seq_range_prev = dict_prev['sequences']
-                if i > 0 and seq_range_prev and dictio and '>>' in dictio[
-                    0]:  # If i > 0 and not empty the current and previous dictionary and the first sequence is split
-                    param = dictio[0].split(' ')
-                    seq_prev = seq_range_prev[-1]
-                    param_seq_prev = seq_prev.split(' ')
-                    if '<->' in seq_prev or '<_>' in seq_prev:
-                        if '<->' in seq_range_prev[-1]:  # If the split was after a space, then there is all id
-                            name_id = param_seq_prev[0].replace('<->', '')
+                if i > 0:
+                    dictio = dict['sequences']
+                    dict_prev = results[i - 1]
+                    seq_range_prev = dict_prev['sequences']
+                    if i > 0 and seq_range_prev and dictio and '>>' in dictio[
+                        0]:  # If i > 0 and not empty the current and previous dictionary and the first sequence is split
+                        param = dictio[0].split(' ')
+                        seq_prev = seq_range_prev[-1]
+                        param_seq_prev = seq_prev.split(' ')
+                        if '<->' in seq_prev or '<_>' in seq_prev:
+                            if '<->' in seq_range_prev[-1]:  # If the split was after a space, then there is all id
+                                name_id = param_seq_prev[0].replace('<->', '')
+                            else:
+                                name_id = param_seq_prev[0].replace('<_>', '') + param[5].replace('^', '')
+                            length = param[4].split('-')[1]
+                            offset_head = param_seq_prev[1]
+                            offset_base = param[3].split('-')[1]
+                            seq_range_prev.pop()  # Remove previous sequence
+                            split = 0
+                            # Update ranges
+                            dict['min_range'] = int(offset_head)
+                            dict_prev['max_range'] = int(offset_head)
                         else:
-                            name_id = param_seq_prev[0].replace('<_>', '') + param[5].replace('^', '')
-                        length = param[4].split('-')[1]
-                        offset_head = param_seq_prev[1]
-                        offset_base = param[3].split('-')[1]
-                        seq_range_prev.pop()  # Remove previous sequence
-                        split = 0
-                        # Update ranges
-                        dict['min_range'] = int(offset_head)
-                        dict_prev['max_range'] = int(offset_head)
-                    else:
-                        length = param[4].split('-')[0]
-                        name_id = param_seq_prev[0]
-                        offset_head = param_seq_prev[2]
-                        offset_base = param[3].split('-')[0]
-                        split = int(param_seq_prev[1])
-                        # Update number of partitions of the sequence
-                        for x in range(i - split, i):  # Update previous sequences
-                            results[x]['sequences'][-1] = results[x]['sequences'][-1].replace(
-                                f' {split} ',
-                                f' {split + 1} ')  # num_chunks_has_divided + 1 (i+1: total of current partitions of sequence)
-                    dictio[0] = dictio[0].replace(f' {param[5]}', '')  # Remove 4rt param
-                    dictio[0] = dictio[0].replace(f' {param[3]} ',
-                                                  f' {offset_base} ')  # [offset_base_0-offset_base_1|offset_base] -> offset_base
-                    dictio[0] = dictio[0].replace(f' {param[4]}', f' {length}')  # [length_0-length_1|length] -> length
-                    dictio[0] = dictio[0].replace(' <X> ', f' {str(split + 1)} ')  # X --> num_chunks_has_divided
-                    dictio[0] = dictio[0].replace(' <Y> ', f' {offset_head} ')  # Y --> offset_head
-                    dictio[0] = dictio[0].replace('>> ', f'{name_id} ')  # '>>' -> name_id
+                            length = param[4].split('-')[0]
+                            name_id = param_seq_prev[0]
+                            offset_head = param_seq_prev[2]
+                            offset_base = param[3].split('-')[0]
+                            split = int(param_seq_prev[1])
+                            # Update number of partitions of the sequence
+                            for x in range(i - split, i):  # Update previous sequences
+                                results[x]['sequences'][-1] = results[x]['sequences'][-1].replace(
+                                    f' {split} ',
+                                    f' {split + 1} ')  # num_chunks_has_divided + 1 (i+1: total of current partitions of sequence)
+                        dictio[0] = dictio[0].replace(f' {param[5]}', '')  # Remove 4rt param
+                        dictio[0] = dictio[0].replace(f' {param[3]} ',
+                                                      f' {offset_base} ')  # [offset_base_0-offset_base_1|offset_base] -> offset_base
+                        dictio[0] = dictio[0].replace(f' {param[4]}', f' {length}')  # [length_0-length_1|length] -> length
+                        dictio[0] = dictio[0].replace(' <X> ', f' {str(split + 1)} ')  # X --> num_chunks_has_divided
+                        dictio[0] = dictio[0].replace(' <Y> ', f' {offset_head} ')  # Y --> offset_head
+                        dictio[0] = dictio[0].replace('>> ', f'{name_id} ')  # '>>' -> name_id
         return results
 
     def __generate_index_file(self, data, file_name):
