14b31e331f09f2576f9277f35824362df7b06512
Sararl27 <sararl2727@gmail.com>
2022-10-14 16:29:14 +0200

    Updating file

--- fastaPartitionerIndex.py
+++ fastaPartitionerIndex.py
@@ -118,6 +118,7 @@ class FastaPartitioner:
                             list_seq[0] = list_prev[-1].replace(f' {param_seq_prev[3]}',
                                                               f' {int(param_seq_prev[3]) + int(param[1])}')  # [length_0-length_1|length] -> length
                             list_prev.pop()
+            results = list(filter(None, results))
         return results
 
     def __generate_index_file(self, data, file_name):
